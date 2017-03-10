4f0ca2d063fc016e3b0c4f65f7ff7e73804d624d
mac <mac@macdeiMac.local>
2017-03-10 10:12:12 +0800

    斗鱼直播框架0.1

---
+++ Douyuzhibo/Douyuzhibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Douyuzhibo
+//
+//  Created by mac on 17/3/10.
+//  Copyright (c) 2017年 小宝哥哥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
